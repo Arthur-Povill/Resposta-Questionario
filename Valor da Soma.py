491489334c74c0a6c04a847efcf9a11d147c01b8
Arthur-Povill <110282004+Arthur-Povill@users.noreply.github.com>
2024-09-25 16:21:55 -0300

    Add files via upload

---
+++ Valor da Soma.py
@@ -0,0 +1,12 @@
+# Inicializando as variáveis
+INDICE = 12
+SOMA = 0
+K = 1
+
+# Loop para calcular a soma
+while K < INDICE:
+    K = K + 1
+    SOMA = SOMA + K
+
+# Imprime o resultado
+print(SOMA)
